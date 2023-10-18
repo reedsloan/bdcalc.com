935ac14549c7e98f0b83f8f30c91377ba8fee79d
Reed Sloan <sloanmobile@gmail.com>
2023-10-18 00:31:57 -0700

    Disabled SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { resolve } from "path"
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   alias: {
     "@": resolve(__dirname, "./")
