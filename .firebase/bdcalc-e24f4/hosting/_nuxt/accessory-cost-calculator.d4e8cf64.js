24d3e1f7582654d4419a2477c061df4471a3be3f
Reed Sloan <sloanmobile@gmail.com>
2023-10-18 16:27:03 -0700

    Updated gitignore

--- .firebase/bdcalc-e24f4/hosting/_nuxt/accessory-cost-calculator.d4e8cf64.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./entry.d7073b00.js";const r={};function t(e,o){return null}const n=c(r,[["render",t]]);export{n as default};
--- .firebase/bdcalc-e24f4/hosting/_nuxt/events.a78830d4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./entry.d7073b00.js";const n={};function o(r,s){return t(),c("div",null," Hello Nuxt 3 ")}const a=e(n,[["render",o]]);export{a as default};
